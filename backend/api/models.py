6bdd85d7e065462b6ca68ebd6b9bec79a3ceec02
Nicholas Ali <etw.business@gmail.com>
2024-04-03 16:15:15 -0400

    Fixed Deployment error

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,8 +6,7 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
 
     def __str__(self):
         return self.title
